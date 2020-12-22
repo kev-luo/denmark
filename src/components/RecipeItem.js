a36859bba45798841977add7ea623a5c889e78df
Kevin Luo <kvn.luo@gmail.com>
2020-12-21 21:10:40 -0500

    refactor list ordering framer motion

--- src/components/RecipeItem.js
+++ src/components/RecipeItem.js
@@ -1,29 +1,44 @@
 import React, { useState } from "react";
 import { ListItem } from "@chakra-ui/react";
-import { motion } from "framer-motion";
+import { motion, useMotionValue } from "framer-motion";
 import { useMeasurePosition } from "../utils/useMeasurePosition";
 
-export default function RecipeItem({ recipe, i, updatePosition, updateOrder }) {
+const onTop = { zIndex: 1 };
+const flat = { zIndex: 0, transition: { delay: 0.3 } };
+const MotionListItem = motion.custom(ListItem);
+
+export default function RecipeItem({ recipe, i, updatePosition, updateOrder, color }) {
   const [isDragging, setDragging] = useState(false);
+  const dragOriginY = useMotionValue(0);
   const ref = useMeasurePosition((pos) => updatePosition(i, pos));
   return (
-    <ListItem p="0" zIndex={isDragging ? 3 : 1}>
-      <motion.div
-        ref={ref}
-        layout
-        initial={false}
-        style={{background: "darkseagreen"}}
-        whileHover={{ scale: 1.03, boxShadow: "0px 3px 3px rgba(0,0,0,0.15)" }}
-        whileTap={{ scale: 1.12, boxShadow: "0px 5px 5px rgba(0,0,0,0.1)" }}
-        drag="y"
-        onDragStart={() => setDragging(true)}
-        onDragEnd={() => setDragging(false)}
-        onViewportBoxUpdate={(_viewportBox, delta) => {
-          isDragging && updateOrder(i, delta.y.translate);
-        }}
-      >
-        {recipe.title}
-      </motion.div>
-    </ListItem>
+    <MotionListItem
+      ref={ref}
+      py={2}
+      px={1}
+      layout
+      initial={false}
+      animate={isDragging ? onTop : flat}
+      style={{ background: `${color}` }}
+      whileHover={{ scale: 1.03, boxShadow: "0px 3px 3px rgba(0,0,0,0.15)" }}
+      whileTap={{ scale: 1.12, boxShadow: "0px 5px 5px rgba(0,0,0,0.1)" }}
+      drag="y"
+      dragOriginY={dragOriginY}
+      dragConstraints={{ top: 0, bottom: 0}}
+      dragElastic={1}
+      onDragStart={() => setDragging(true)}
+      onDragEnd={() => setDragging(false)}
+      positionTransition={({ delta }) => {
+        if(isDragging) {
+          dragOriginY.set(dragOriginY.get() + delta.y);
+        }
+        return !isDragging
+      }}
+      onViewportBoxUpdate={(_viewportBox, delta) => {
+        isDragging && updateOrder(i, delta.y.translate);
+      }}
+    >
+      {recipe.title}
+    </MotionListItem>
   );
 }
--- src/pages/recipes.js
+++ src/pages/recipes.js
@@ -8,7 +8,12 @@ import { Container } from "../components/Container";
 import RecipeItem from "../components/RecipeItem";
 import { usePositionReorder } from "../utils/usePositionReorder";
 
-const items = [60,80,70,100]
+const colors = [
+  "linear-gradient(135deg, #f6d365 0%, #fda085 100%)",
+  "linear-gradient(135deg, #f093fb 0%, #f5576c 100%)",
+  "linear-gradient(135deg, #5ee7df 0%, #b490ca 100%)",
+  "linear-gradient(135deg, #c3cfe2 0%, #c3cfe2 100%)",
+];
 
 export default function Recipes({ allRecipes }) {
   const router = useRouter();
@@ -18,16 +23,20 @@ export default function Recipes({ allRecipes }) {
       <Head>
         <title>{router.pathname.split("/")[1]}</title>
       </Head>
-      <List spacing={3} >
-        {order.map((recipe, index) => (
-          <RecipeItem 
-          key={recipe.id} 
-          recipe={recipe} 
-          i={index}
-          updatePosition={updatePosition}
-          updateOrder={updateOrder}
-          />
-        ))}
+      <List spacing={3}>
+        {order.map((recipe, index) => {
+          const color = colors[Math.floor(Math.random() * colors.length)];
+          return (
+            <RecipeItem
+              key={recipe.id}
+              recipe={recipe}
+              i={index}
+              updatePosition={updatePosition}
+              updateOrder={updateOrder}
+              color={color}
+            />
+          );
+        })}
       </List>
     </Container>
   );
