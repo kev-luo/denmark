e3555076e08b9d3aaaf44356bf96571db3266a0f
Kevin Luo <kvn.luo@gmail.com>
2020-12-24 15:15:18 -0500

    create 2048 board

--- src/pages/funzone/2048.js
+++ src/pages/funzone/2048.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useState } from "react";
+import { GridItem, SimpleGrid, Box, Center } from "@chakra-ui/react";
 
 import { Container } from "../../components/Container";
 
 export default function TwentyFourtyEight() {
+  const initialBoard = [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+  ];
+
+  const [board, setBoard] = useState(initialBoard);
   return (
     <Container>
-      2048
+      <GridItem justifySelf="center">
+        <SimpleGrid columns={4} spacing={1} border="1px solid green" w={400} mt={10}>
+          {board.flat().map((items, index) => (
+            <Center key={index} border="1px solid black" h={90} >Hi</Center>
+          ))}
+        </SimpleGrid>
+      </GridItem>
     </Container>
-  )
+  );
 }
