2c534199e154b364fc33004ccc5cfc0fcd6a962e
Kevin Luo <kvn.luo@gmail.com>
2020-12-25 16:26:22 -0500

    update fun zone layout to match container

--- src/pages/funzone.js
+++ src/pages/funzone.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Heading, List, GridItem } from "@chakra-ui/react";
+import { Heading, List, Flex, Box } from "@chakra-ui/react";
 import Image from "next/image";
 import { Container } from "../components/Container";
 import FunZoneAccordion from "../components/FunZoneAccordion";
@@ -10,22 +10,35 @@ const MotionList = motion.custom(List);
 
 export default function FunZone() {
   return (
-    <Container title="Fun Zone" cols="repeat(2, 1fr)" rows="repeat(10, 1fr)">
-      <GridItem rowSpan={1} colSpan={2} justifySelf="center">
-        <Heading mt={10}>Fun Zone: You Best Have Fun</Heading>
-      </GridItem>
-      <GridItem rowSpan={9} colSpan={1} justifySelf="center" alignSelf="center">
-        <AnimateSharedLayout>
-          <MotionList layout="true" initial={{ borderRadius: 25 }} spacing={2}>
-            {games.map((game) => (
-              <FunZoneAccordion game={game} key={game.id} />
-            ))}
-          </MotionList>
-        </AnimateSharedLayout>
-      </GridItem>
-      <GridItem rowSpan={9} colSpan={1} justifySelf="start" alignSelf="start">
-        <Image src="/funGuy.png"  layout="intrinsic" width={400} height={600}/>
-      </GridItem>
+    <Container title="Fun Zone">
+      <Box textAlign="center" py={4}>
+        <Heading>Fun Zone: You Best Have Fun</Heading>
+      </Box>
+      <Flex justifyContent="space-around" w="40%" m="auto">
+        <Box alignSelf="center" w="400px">
+          <AnimateSharedLayout>
+            <MotionList
+              layout="true"
+              initial={{ borderRadius: 25 }}
+              spacing={2}
+            >
+              <Flex direction="column">
+                {games.map((game) => (
+                  <FunZoneAccordion game={game} key={game.id} />
+                ))}
+              </Flex>
+            </MotionList>
+          </AnimateSharedLayout>
+        </Box>
+        <Box>
+          <Image
+            src="/funGuy.png"
+            layout="intrinsic"
+            width={400}
+            height={600}
+          />
+        </Box>
+      </Flex>
     </Container>
   );
 }
