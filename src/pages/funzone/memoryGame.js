05201e964106e097aa841f0e1a0c8b965e77d0af
Kevin Luo <kvn.luo@gmail.com>
2020-12-24 21:41:48 -0500

    display memory game images

--- src/pages/funzone/memoryGame.js
+++ src/pages/funzone/memoryGame.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import Image from "next/image";
+import { GridItem, Grid, Box } from "@chakra-ui/react";
 
+import { images } from "../../../data/memoryGame";
 import { Container } from "../../components/Container";
 
 export default function memoryGame() {
   return (
     <Container>
-      Memory Game
+      <Grid w="50%" h="50%" templateColumns="repeat(3, 1fr)" templateRows="repeat(4, 1fr)" gap={2} mx="auto" my={10}>
+        {images.map(image => (
+          <GridItem key={image.id} border="1px solid black">
+            <Image src={image.url} width={200} height={200} />
+          </GridItem>
+        ))}
+      </Grid>
+      <Box justifySelf="center">
+        Score: 
+      </Box>
     </Container>
   )
 }
