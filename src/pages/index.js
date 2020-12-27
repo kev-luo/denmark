3aca9e418119d6ecb9e3058854c9130afd003194
Kevin Luo <kvn.luo@gmail.com>
2020-12-27 00:45:59 -0500

    home page animation

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,12 @@ import { CheckCircleIcon, LinkIcon } from "@chakra-ui/icons";
 import {
   Center,
   Heading,
-  Link as ChakraLink,
   List,
   ListIcon,
   ListItem,
   Text,
   Box,
+  Link as ChakraLink,
 } from "@chakra-ui/react";
 import Link from "next/link";
 import Image from "next/image";
@@ -19,12 +19,14 @@ import { Container } from "../components/Container";
 import { Footer } from "../components/Footer";
 
 const MotionBox = motion.custom(Box);
+const MotionContainer = motion.custom(Box);
+const MotionImage = motion.custom(Box);
 
 const boxVariants = {
   hidden: {
     x: "-5rem",
     opacity: 0,
-    rotate: [90]
+    rotate: [90],
   },
   visible: {
     x: 0,
@@ -32,6 +34,31 @@ const boxVariants = {
     transition: { type: "spring", stiffness: 500, damping: 30 },
   },
 };
+const containerVariants = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.08
+    }
+  }
+}
+
+const imgVariants = {
+  hidden: {
+    opacity: 0,
+    y: -100
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+  }
+}
+
+const imgIds = [0, 1, 2, 3, 4, 5, 6];
+const initialLeft = -25.2;
 
 export default function Home() {
   const [display, setDisplay] = useState(false);
@@ -54,10 +81,31 @@ export default function Home() {
 
   return (
     <Container title="Home">
-      <Center py={10}>
+      <MotionContainer h="4rem" w="50%" mx="auto" pos="relative" top={20} variants={containerVariants} initial="hidden" animate="visible">
+        <Box h="20%" w="100%" bgColor="black" />
+        {imgIds.map((id) => {
+          return (
+            <MotionImage
+              pos="absolute"
+              h="200%"
+              w="100%"
+              top="-5.2rem"
+              left={`${initialLeft + 8.4 * id}rem`}
+              variants={imgVariants} 
+            >
+              <Image
+                src="/stunnahShades.png"
+                layout="fill"
+                objectFit="contain"
+              />
+            </MotionImage>
+          );
+        })}
+      </MotionContainer>
+      <Center py={20}>
         <Heading>Food + Fun + Mystery ft. Tacococo</Heading>
       </Center>
-      <Center my={5}>
+      <Center my={0}>
         <AnimateSharedLayout>
           <List spacing={8} my={0} fontSize="1.5rem">
             {sitePages.map((page) => {
