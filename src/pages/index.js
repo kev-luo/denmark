3bf0eb0185c0aa0e026df13e15f300574598a356
Kevin Luo <kvn.luo@gmail.com>
2020-12-26 21:57:48 -0500

    refactor home page animation hover

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ import {
 } from "@chakra-ui/react";
 import Link from "next/link";
 import Image from "next/image";
-import { motion } from "framer-motion";
+import { motion, AnimateSharedLayout } from "framer-motion";
 
 import { sitePages } from "../../data/pages";
 import { Container } from "../components/Container";
@@ -22,10 +22,14 @@ const MotionBox = motion.custom(Box);
 
 const boxVariants = {
   hidden: {
+    x: "-5rem",
     opacity: 0,
+    rotate: [90]
   },
   visible: {
+    x: 0,
     opacity: 1,
+    transition: { type: "spring", stiffness: 500, damping: 30 },
   },
 };
 
@@ -54,37 +58,38 @@ export default function Home() {
         <Heading>Food + Fun + Mystery ft. Tacococo</Heading>
       </Center>
       <Center my={5}>
-        <List spacing={8} my={0} fontSize="1.5rem">
-          {sitePages.map((page) => {
-            return (
-              <ListItem
-                position="relative"
-                onMouseEnter={(e) => handleHover(page.alias, e)}
-                onMouseLeave={(e) => handleHover(page.alias, e)}
-              >
-                {display[page.alias] && (
-                  <MotionBox
-                    position="absolute"
-                    transform="rotate(90deg)"
-                    left="-6rem"
-                    top="-3rem"
-                    variants={boxVariants}
-                    initial="hidden"
-                    animate="visible"
-                  >
-                    <Image src="/attention.png" width={45} height={120} />
-                  </MotionBox>
-                )}
-                <ListIcon as={CheckCircleIcon} color="green.500" />
-                <Link href={`${page.path}`}>
-                  <ChakraLink flexGrow={1} mr={2}>
-                    {page.name} <LinkIcon />
-                  </ChakraLink>
-                </Link>
-              </ListItem>
-            );
-          })}
-        </List>
+        <AnimateSharedLayout>
+          <List spacing={8} my={0} fontSize="1.5rem">
+            {sitePages.map((page) => {
+              return (
+                <ListItem
+                  position="relative"
+                  onMouseEnter={(e) => handleHover(page.alias, e)}
+                  onMouseLeave={(e) => handleHover(page.alias, e)}
+                >
+                  {display[page.alias] && (
+                    <MotionBox
+                      position="absolute"
+                      left="-6rem"
+                      top="-3rem"
+                      variants={boxVariants}
+                      initial="hidden"
+                      animate="visible"
+                    >
+                      <Image src="/attention.png" width={45} height={120} />
+                    </MotionBox>
+                  )}
+                  <ListIcon as={CheckCircleIcon} color="green.500" />
+                  <Link href={`${page.path}`}>
+                    <ChakraLink flexGrow={1} mr={2}>
+                      {page.name} <LinkIcon />
+                    </ChakraLink>
+                  </Link>
+                </ListItem>
+              );
+            })}
+          </List>
+        </AnimateSharedLayout>
       </Center>
 
       <Footer>
